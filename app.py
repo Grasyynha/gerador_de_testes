36b84c1288e4efac7e659fd8c896334f23d234ed
Grasiela Texeira <grasy.mst@gmail.com>
2021-11-30 10:30:39 -0300

    Adicionando dados

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print('Projeto de teste')
+Bem vindo ao gerador de testes - Para finalizar o programa, digite "parar"
+------------------------------
+Escolhe uma ou mais opções abaixo a serem geradas aleatóriamente
+[1] - NOME
+[2] - E-MAIL
+[3] - Telefone
+[4] - Cidade
+[5] - Estado
+Digite uma(as) opções:
+
+
+Gostaria de salvar os dados em um arquivo de texto?(s/n)
+
+
+encerrando o programa
